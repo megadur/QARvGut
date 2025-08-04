608ff56cd1581be764ff7b033cd1b1d2b29050db
megadur <megadur@gmail.com>
2025-08-04 16:50:37 +0200

    Add project files.

---
+++ QARvGut/QARvGut.Core/Services/Shop/Interfaces/ICustomerService.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------
+// Email: quickapp@ebenmonney.com
+// Templates: www.ebenmonney.com/templates
+// (c) 2024 www.ebenmonney.com/mit-license
+// ---------------------------------------
+
+using QARvGut.Core.Models.Shop;
+
+namespace QARvGut.Core.Services.Shop
+{
+    public interface ICustomerService
+    {
+        IEnumerable<Customer> GetTopActiveCustomers(int count);
+        IEnumerable<Customer> GetAllCustomersData();
+    }
+}
---
+++ QARvGut/QARvGut.Server/Services/UserIdAccessor.cs
@@ -0,0 +1,30 @@
+﻿// ---------------------------------------
+// Email: quickapp@ebenmonney.com
+// Templates: www.ebenmonney.com/templates
+// (c) 2024 www.ebenmonney.com/mit-license
+// ---------------------------------------
+
+using System.Security.Claims;
+using QARvGut.Core.Services.Account;
+using static OpenIddict.Abstractions.OpenIddictConstants;
+
+namespace QARvGut.Server.Services
+{
+    public class UserIdAccessor(IHttpContextAccessor httpContextAccessor) : IUserIdAccessor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+        public string? GetCurrentUserId() => _httpContextAccessor.HttpContext?.User.FindFirstValue(Claims.Subject);
+    }
+
+    public class SystemUserIdAccessor : IUserIdAccessor
+    {
+        private readonly string? id;
+
+        private SystemUserIdAccessor(string? id) => this.id = id;
+
+        public string? GetCurrentUserId() => id;
+
+        public static SystemUserIdAccessor GetNewAccessor(string? id = "SYSTEM") => new(id);
+    }
+}
